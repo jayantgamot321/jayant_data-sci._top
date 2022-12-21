f215a24f504f8aa88c20101d77415babd112d35d
jayantgamot321 <jayantgamot11@gmail.com>
2022-12-21 13:05:59 +0530

    Create 16-12-22.py

---
+++ 16-12-22.py
@@ -0,0 +1,30 @@
+print("welcome to python programmig")
+
+
+a=10
+
+print(type (a))
+"""
+
+a=554689978714678
+
+b=32116587874541
+
+print("a = ",(a), "b = ", (b))
+
+print("addition =", (a+b))
+
+print("addition =", (a-b))
+
+print ("addition =", (a*b) )
+
+print ("addition =", (a/b))
+       
+
+a = int ( input("enter a : ") )
+
+print("A = " , a)
+
+print (type (a))
+
+"""
