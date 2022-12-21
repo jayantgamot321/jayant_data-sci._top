a652c4200af1e56f170db10507bb7f71988077e3
jayantgamot321 <jayantgamot11@gmail.com>
2022-12-21 13:05:47 +0530

    Create loop.py

---
+++ loop.py
@@ -0,0 +1,17 @@
+"""
+n = int(input("enter N : "))
+
+while n>0:
+    print("tops tech")
+    n = n-1
+
+n = int(input("enter N :"))
+sum=0
+
+while n>0:
+    sum = sum+n
+    n=n-1
+
+print("sum : ",sum)    
+
+"""
