325be375d723e138a3c5af5dbca9939f39318755
jayantgamot321 <jayantgamot11@gmail.com>
2022-12-21 13:05:51 +0530

    Create forloopdemo.py

---
+++ forloopdemo.py
@@ -0,0 +1,30 @@
+"""
+ 1)
+ 
+for i in range(20):
+    print(i)
+2)
+
+
+for i in range(1,11):
+    print(i)
+
+n= int(input("enter n "))
+
+for i in range(1,n+1):
+    print(i)
+
+3)
+
+
+
+for i in range(3,10,2):
+    print(i)
+
+4)
+
+
+for i in range(41,0,-1):
+    print(i)
+        
+"""
