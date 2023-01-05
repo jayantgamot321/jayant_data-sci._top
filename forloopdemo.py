06dc0f1b5bf1ba0a0deee215214aa9d1a8af9f58
jayantgamot321 <jayantgamot11@gmail.com>
2023-01-05 14:59:14 +0530

    Update forloopdemo.py

--- forloopdemo.py
+++ forloopdemo.py
@@ -1,30 +1,75 @@
 """
- 1)
- 
-for i in range(20):
-    print(i)
-2)
-
-
-for i in range(1,11):
+for i in range(10):
     print(i)
 
-n= int(input("enter n "))
+for i in range(1,10):
+    print(i)
+    
+
+n = int(input("enter N = "))
 
 for i in range(1,n+1):
     print(i)
 
-3)
 
-
-
-for i in range(3,10,2):
+for i in range(1,15,2):
     print(i)
 
-4)
 
-
-for i in range(41,0,-1):
+for i in range(15,0,-1):
     print(i)
-        
+
+
+n = int(input("enter N : "))
+sum=0
+
+while n>0:
+    sum=sum+n
+    n = n-1
+print("sum = ",sum)    
+
+
+n = int(input("enter N "))
+sum=0
+for i in range(1,n+1):
+    sum = sum+i
+print("sum = ",sum)    
 """
+n = int(input("enter N "))
+evensum=0
+oddsum=0
+
+for i in range(1,n+1):
+    if i%2==0:
+        evensum = evensum+i
+    else:
+        oddsum = oddsum+i
+print("evensum = ",evensum)
+print("oddsum = ",oddsum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
