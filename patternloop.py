c8304b8f301725d1f1cb6b38ecf89773736711d7
jayantgamot321 <jayantgamot11@gmail.com>
2023-01-05 14:58:49 +0530

    Create patternloop.py

---
+++ patternloop.py
@@ -0,0 +1,20 @@
+"""
+for i in range(10):
+    print("*"*i)
+
+for i in range(1,10):
+    print(" "*(9-i),"*"*i)
+
+for i in range(10,0,-1):
+    print(" "*(9-i)," *"*i)
+    
+for i in range(1,10):
+    print(" "*(9-i)," *"*i)
+for i in range(10,0,-1):
+    print(" "*(9-i)," *"*i)
+"""
+s = "tops tecnologies"
+
+
+print(s.center(70,"*"))
+print(s.capitalize())
