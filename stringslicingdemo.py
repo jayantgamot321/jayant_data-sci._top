bea14bcfd18aa4b348169f80ab4955f173fcd66a
jayantgamot321 <jayantgamot11@gmail.com>
2023-01-05 14:58:38 +0530

    Create stringslicingdemo.py

---
+++ stringslicingdemo.py
@@ -0,0 +1,16 @@
+ #   012345678901234567  +
+s = "python programming"
+ #   876543210987654321  -
+
+
+print(s[:10])    
+print(s[1:10])
+print(s[10:])
+print(s[1:15:2])
+print(s[::])
+
+
+print(s[:-5])
+print(s[-8:])
+print(s[-7:-14:-1])
+print(s[::-1])
