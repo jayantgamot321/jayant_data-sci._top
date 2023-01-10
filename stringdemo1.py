a2b319b3cfa31724a03d325a51a0ec07cf4fb419
jayantgamot321 <jayantgamot11@gmail.com>
2023-01-10 19:09:18 +0530

    Create stringdemo1.py

---
+++ stringdemo1.py
@@ -0,0 +1,47 @@
+s = "Tops Technologies"
+#print(s)
+
+#for i in range(10):
+#    print(s)
+
+print(s.center(50,"*"))
+print(s.capitalize())
+print(s.casefold())
+print(s.find("ie"))
+print(s.endswith("es"))
+print(s.startswith("t"))
+print(s.upper())
+print(s.istitle())
+print("tops".isalnum())
+print("tops123".isalnum())
+print("123".isalpha())
+print("123".isdigit())
+print("tops".isnumeric())
+print("hello".replace("l","w"))
+print(s.swapcase())
+print("python programming".title())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+
+
+
+
+
