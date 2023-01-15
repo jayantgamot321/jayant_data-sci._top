3c729d80109e40a48f85ef4859ff291dfed9cf13
jayantgamot321 <jayantgamot11@gmail.com>
2023-01-15 19:18:11 +0530

    Create stringslicingdemo.py

---
+++ CLASSWORK/stringslicingdemo.py
@@ -0,0 +1,42 @@
+
+
+#    012345678901234567  +
+#s = "Python Programming"
+#    876543210987654321  -
+
+
+s = "Tops Technologies"
+
+print(s)
+
+# positive values
+print(s[:10])
+print(s[11:])
+print(s[2:16])
+print(s[1:17:2])
+print(s[::])
+
+#negative values
+
+print(s[:-5])
+print(s[-8:])
+print(s[-7:-14])
+print(s[-2:-16:-2])
+print(s[::-1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
