e967cbe710d9cf4657486317de112a4bf404dda9
jayantgamot321 <jayantgamot11@gmail.com>
2023-01-15 19:17:50 +0530

    Create loopdemo.py

---
+++ CLASSWORK/loopdemo.py
@@ -0,0 +1,62 @@
+"""
+        Loop : it is a process where a task repeats again and again.
+
+        e.g Circle
+
+        3 important facts in loop.
+
+        ->Initialization
+        ->Conditional
+        ->Updation (incr/decr)
+
+        while loop
+        for loop
+
+
+1) While Loop
+
+    while condition:
+        statement
+        updation
+
+"""
+
+n = int(input("Enter N : "))
+sum=0
+
+while n>0:
+    sum = sum+n #28
+    n = n-1 #0
+
+print("Sum : ",sum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
