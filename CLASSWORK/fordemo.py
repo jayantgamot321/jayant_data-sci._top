9793d6efba0701c89e55c4baae836238699ccdd1
jayantgamot321 <jayantgamot11@gmail.com>
2023-01-15 19:17:16 +0530

    Create fordemo.py

---
+++ CLASSWORK/fordemo.py
@@ -0,0 +1,83 @@
+"""
+
+In C,C++,Java syntax
+
+for(int i=0;i<10;i++)
+{
+    statement
+}
+
+In Python the syntax is changed drastically.
+
+for i in range():
+    statement
+
+i - > it is a variable, Default Value of i is 0.
+
+range(start,end,updation).
+
+There are 3 parameters in range function.
+
+-> Start
+-> End
+-> Update (incr,decr) -> Default is Incr
+
+There are 4 Variations in For Loop.
+
+1) 
+
+
+for i in range(10):
+    print(i)
+
+
+2)
+
+for i in range(1,11):
+    print(i)
+
+
+
+n = int(input("Enter N "))
+
+for i in range(1,n+1):
+    print(i)
+
+
+3)
+
+for i in range(3,10,2):
+    print(i)
+
+4)
+
+"""
+
+for i in range(10,0,-1):
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
