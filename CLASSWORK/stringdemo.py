54d262385cacc3f823d6effccf205a144d094c84
jayantgamot321 <jayantgamot11@gmail.com>
2023-01-15 19:18:04 +0530

    Create stringdemo.py

---
+++ CLASSWORK/stringdemo.py
@@ -0,0 +1,21 @@
+""""
+
+#    012345678901234  +
+s = "tops tecnologis"
+#    543210987654321   -
+
+
+print(s)
+
+print(s[:10])
+print(s[1:10])
+print(s[1:14:2])
+
+print(s[:-5])
+print(s[::-1])
+print(s[-1:-16:-1])
+print(s[-5:-14:-1])
+print(s[-2:15:-2])
+
+"""
+
