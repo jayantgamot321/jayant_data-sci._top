ee9e3c41e29c88d58d71defca88fcf714a6e90f6
jayantgamot321 <jayantgamot11@gmail.com>
2023-01-15 19:17:41 +0530

    Create patternusingloop.py

---
+++ CLASSWORK/patternusingloop.py
@@ -0,0 +1,34 @@
+"""
+
+1) Pattern 1
+
+for i in range(1,10):
+    print("*"*i)
+
+2) Pattern 2
+for i in range(1,10):
+    print(" "*(9-i),"*"*i)
+
+#3) Pattern 3
+for i in range(1,10):
+    print(" "*(9-i)," *"*i)
+
+4) Pattern 4
+
+for i in range(9,0,-1):
+    print(" "*(9-i)," *"*i)
+
+5) Pattern 5
+
+for i in range(1,10):
+    print(" "*(9-i)," *"*i)
+for i in range(9,0,-1):
+    print(" "*(9-i)," *"*i)
+
+"""
+
+
+
+
+
+
