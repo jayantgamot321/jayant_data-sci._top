081a676d1cc36cbecf5523372e592648472edfb9
jayantgamot321 <120360408+jayantgamot321@users.noreply.github.com>
2023-02-16 10:52:33 +0530

    Create Inheritancedemo.py

---
+++ CLASSWORK/Inheritancedemo.py
@@ -0,0 +1,44 @@
+"""
+    Inheritance : It is a concept, where a derived class accessing the properties
+                  of the base class.
+
+                - Code Reusability.
+
+    practical def : It is creating a new class from an already existing class.
+
+
+    Imp note :
+
+    In Inheritance, Object will always be created of derived class.
+
+
+    Types of Inheritance
+
+    1) Single-level Inheritance
+    2) Multi-level Inheritance
+    3) Hierarchical-level Inheritance
+    4) Multiple-level Inheritance
+    5) Hybrid-level Inheritance
+    
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
