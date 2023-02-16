eba8ee69f4a150e0fdf337b988810b024df1ea9f
jayantgamot321 <120360408+jayantgamot321@users.noreply.github.com>
2023-02-16 10:53:01 +0530

    Create PolymorphismDemo.py

---
+++ CLASSWORK/PolymorphismDemo.py
@@ -0,0 +1,36 @@
+"""
+    Polymorphism :  it is a combination of multiple forms.
+
+    Poly -> Many
+    morphism -> Forms
+
+    example : Bird.
+
+             Reaction
+              
+
+    There are 2 types of it.
+
+    1) Compile Time Polymorphsim (Static Polymorphsim) or (Early Binding Polymorphism)
+
+        e.g Method Overloading
+
+            - Different return-type
+            - Same Function-name
+            - Different parameters/arguments
+            
+    
+    2) Run Time Polymorphism    (Dynamic Polymorphism) or (Late Binding Polymorphism)
+
+        e.g Method Overriding
+
+            - Same return-type
+            - Same function-name
+            - Same arguments
+
+            -Method Signature same.
+
+
+        - Python does not support compile time polymorphsim. ie (Method Overloading)
+
+"""
