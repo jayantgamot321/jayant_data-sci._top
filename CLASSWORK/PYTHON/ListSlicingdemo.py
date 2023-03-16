14f3da25eb5241491954d8456ddeb4866a849034
jayantgamot321 <120360408+jayantgamot321@users.noreply.github.com>
2023-03-16 12:40:52 +0530

    Create ListSlicingdemo.py

---
+++ CLASSWORK/PYTHON/ListSlicingdemo.py
@@ -0,0 +1,23 @@
+
+#    0 1  2   3    4      5        6   7  8    9  0     1    2  3   4    +
+l = [1,2,1.1,2.2,"python",False,"Tops",0,True,1.1,2000,1200,3.3,50,1.1]
+#    5 4  3   2    1       0      9    8  7    6   5    4    3  2   1   -
+print(l)
+
+#for i in l:
+#    print(i)
+
+#print("Tops" in l)
+
+print(l[:])
+print(l[:11])
+print(l[5:])
+print(l[3:14])
+print(l[1:10:3])
+
+print(l[:-5])
+print(l[-8:])
+print(l[-7:-14])
+print(l[-1:-14:-1])
+
+
