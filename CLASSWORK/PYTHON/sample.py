1390210d2bfc88b3241f5945752dbf1bd72966ee
jayantgamot321 <120360408+jayantgamot321@users.noreply.github.com>
2023-03-16 12:39:53 +0530

    Create sample.py

---
+++ CLASSWORK/PYTHON/sample.py
@@ -0,0 +1,44 @@
+"""
+# -> Single Line Comment
+
+ 
+"""
+"""
+print("Welcome to Python Programming",end="")
+print("In Tops Technologies")
+
+n = 98
+print("N = ",n)
+print(type(n))
+"""
+
+#n = 87899989009889.5454
+#print(type(n))
+"""
+a = 50454545345345
+b = 413453453453454543
+
+print("A = ",a,"B = ",b);
+
+print("Addition = ",(a+b))
+print("Subtraction = ",(a-b))
+print("Multiplication = ",(a*b))
+print("Division = ",(a/b))
+"""
+
+a = int(input("Enter A : "))
+print("A = ",a)
+print(type(a))
+
+
+
+
+
+
+
+
+
+
+
+
+
